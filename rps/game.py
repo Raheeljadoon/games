5b576df9aa2a9eb12ad3bc39c0a429be57bb8207
raheeljadoon <raheeljadoon56@gmail.com>
2021-06-23 07:48:54 -0400

    checklk

--- rps/game.py
+++ rps/game.py
@@ -4,4 +4,3 @@ player_1 = input("choose any one from these three rock , paper or scissor  ")
 player_choises = ["rock","paper","scissor"]
 player_2 =random.choice(player_choises) 
 print(player_2)
-if 
