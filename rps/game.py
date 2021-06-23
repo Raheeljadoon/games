eb2d070ea8cbcff362aa3c782d3cc561f4d36150
raheeljadoon <raheeljadoon56@gmail.com>
2021-06-23 08:12:30 -0400

    update

--- rps/game.py
+++ rps/game.py
@@ -3,4 +3,14 @@ print("rock paper scissor game")
 player_1 = input("choose any one from these three rock , paper or scissor  ")
 player_choises = ["rock","paper","scissor"]
 player_2 =random.choice(player_choises) 
-print(player_2)
+print("player_2 choose" , player_2)
+
+if(player_1 =="rock" and player_2 == "rock"):
+   
+    print("tie")
+elif(player_1 == "rock" and player_2 == "paper"):
+    print("player_2 wins")
+
+elif(player_1 == "rock" and player_2 == "scissor"):
+    print("player_1 wins")
+   
