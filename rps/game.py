3bc685cfc69b09ba6c39f6ff1e15548736d19e2e
raheeljadoon <raheeljadoon56@gmail.com>
2021-06-24 01:12:05 -0400

    update

--- rps/game.py
+++ rps/game.py
@@ -1,36 +1,42 @@
 import random
-print("rock paper scissor game")
-player_1 = input("choose any one from these three rock , paper or scissor  ")
-player_choises = ["rock","paper","scissor"]
-player_2 =random.choice(player_choises) 
-print("player_2 choose" , player_2)
+while True:
+    print("rock paper scissor game")
+    player_1 = input("choose any one from these three rock , paper or scissor  ")
+    print("you chooser ", player_1)
+    player_choises = ["rock","paper","scissor"]
+    player_2 =random.choice(player_choises) 
+    print("player_2 choose" , player_2)
 
-if(player_1 =="rock" and player_2 == "rock"):
-   
-    print("tie")
-elif(player_1 == "rock" and player_2 == "paper"):
-    print("player_2 wins")
+    if(player_1 =="rock" and player_2 == "rock"):
+    
+        print("tie")
+    elif(player_1 == "rock" and player_2 == "paper"):
+        print("player_2 wins")
 
-elif(player_1 == "rock" and player_2 == "scissor"):
-    print("player_1 wins")
+    elif(player_1 == "rock" and player_2 == "scissor"):
+        print("player_1 wins")
 
-if(player_1 == "paper" and player_2 == "scissor"):
-    print("player_2 wins")
+    if(player_1 == "paper" and player_2 == "scissor"):
+        print("player_2 wins")
 
-elif(player_1 == "paper" and player_2 == "rock"):
-    print("player_1 wins")
+    elif(player_1 == "paper" and player_2 == "rock"):
+        print("player_1 wins")
 
-elif(player_1 == "paper" and player_2 == "paper"):
-    print("tie")
+    elif(player_1 == "paper" and player_2 == "paper"):
+        print("tie")
 
-if(player_1 == "scissor" and player_2 == "paper"):
-    print("player_1 wins")
+    if(player_1 == "scissor" and player_2 == "paper"):
+        print("player_1 wins")
 
-elif(player_1 == "scissor" and player_2 == "scissor"):
-    print("tie")
+    elif(player_1 == "scissor" and player_2 == "scissor"):
+        print("tie")
 
-elif(player_1 == "scissor" and player_2 == "rock"):
-    print("player_2 wins")
+    elif(player_1 == "scissor" and player_2 == "rock"):
+        print("player_2 wins")
+    play_again = input("do you want top play again y/n")
+    if play_again != "y":
+        break
+
 
    
 
