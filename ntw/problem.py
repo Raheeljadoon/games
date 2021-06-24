713a84c29f11f58a79b31b7dec4d099ea080af07
raheeljadoon <raheeljadoon56@gmail.com>
2021-06-24 06:24:58 -0400

    base

--- ntw/problem.py
+++ ntw/problem.py
@@ -1,6 +1,7 @@
-print("number converts into words" )
-num_input = int(input("enter any digit you want "))
-
-num_1 = {1:'one',2:'two'}
-print(num_1[num_input])
-   
+single_num = {1 :'one', 2 :'two', 3:'three', 4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine',10:'ten',11:'eleven',12:'twelve',13:'thirteen',14:'fourteen',15:'fiften',16:'sixteen',17:'seventeen',18:'eighteen',19:'nineteen'}
+double_num = {20:'twenty',30:'thirty',40:'fourty',50:'fifty',60:'sixty',70:'seventy',80:'eighty',90:'ninty'}
+num = int(input("enter any number"))
+if 0 < num < 20:
+    print(single_num[num])
+elif 19 < num < 100:
+    print(double_num[num])
