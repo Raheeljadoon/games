64600202e52be2423391e64a1761459c9bb87316
raheeljadoon <raheeljadoon56@gmail.com>
2021-06-28 07:58:47 -0400

    update

--- rps_oop/code.py
+++ rps_oop/code.py
@@ -17,23 +17,28 @@ class Game():
     def compare_choice(self,player_1,player_2):
         if player_1 == "rock" and player_2 == "paper" or player_1 == "paper" and player_2 == "scissor":
 
-            return "player_2 wins"
+            return "player_2 "
     
         elif player_1 == "rock" and player_2 == "scissor" or player_1 == "scissor" and player_2 == "paper":
-            return "player_1 wins"
+            return "player_1 "
             
         elif player_1 == player_2:
-            return "its tie"
+            return "tie"
 
     def play(self):
         player_1_ch = player_1.choose()
         player_2_ch = player_2.choose()
         result = self.compare_choice(player_1_ch,player_2_ch)
-        if result == "player_2 wins":
+        if result == "player_2 ":
             print("player_1 choose",player_1_ch ,"and player_2 choose" , player_2_ch)
-
-        elif  result == "player_1 wins":
+            print("player_2 wins")
+        elif  result == "player_1 ":
             print("player_1 choose",player_1_ch ,"and player_2 choose" , player_2_ch) 
+            print("player_1 wins")
 
+        elif result == "tie":
+            
+            print("player_1 choose",player_1_ch ,"and player_2 choose" , player_2_ch)
+            print("both choose same its tie")
 game = Game()
 game.play()
