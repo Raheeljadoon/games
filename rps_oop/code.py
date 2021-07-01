aee43ef837d3d9d51788040fc42f28a344f48882
raheeljadoon <raheeljadoon56@gmail.com>
2021-07-01 17:31:06 +0500

    new

--- rps_oop/code.py
+++ rps_oop/code.py
@@ -1,30 +1,42 @@
 import random
+
 choicess = ["rock","paper","scissor"]
 
-class player_1():
-    
+class player_1:
+        
     def choose() :
-        choicess = ["rock","paper","scissor"]
-        player_1_choose = random.choice(choicess)
-        return player_1_choose
+        
+        player_1_choose = input("choose from rock , paper and scissor  : ")
+        if player_1_choose in choicess :
+            return player_1_choose
 
-class player_2():
+        else :
+            return "wrong input"
+
+
+class player_2:
     def choose():
         player_2_choose = random.choice(choicess)
         return player_2_choose
 
-class Game():
+class Game:
+    
+    def __init__(self) :
+
+        pass
     def compare_choice(self,player_1,player_2):
         if player_1 == "rock" and player_2 == "paper" or player_1 == "paper" and player_2 == "scissor" or player_1 == "scissor" and player_2 == "rock":
             return "player_2 "
-    
+        
         elif player_1 == "rock" and player_2 == "scissor" or player_1 == "scissor" and player_2 == "paper" or player_1 =="paper" and player_2 == "rock":
             return "player_1 "
-            
+                
         elif player_1 == player_2:
             return "tie"
+       
 
     def play(self):
+        
         player_1_ch = player_1.choose()
         player_2_ch = player_2.choose()
         result = self.compare_choice(player_1_ch,player_2_ch)
@@ -37,10 +49,22 @@ class Game():
             print("player_1 choose",player_1_ch ,"and player_2 choose" , player_2_ch) 
             print("player_1 wins")
 
-        else:
-            result == "tie"
+        elif result == "tie":
             print("player_1 choose",player_1_ch ,"and player_2 also choose" , player_2_ch)
             print("both choose same its tie")
-            
-game = Game()
+
+        else : 
+            print("wrong input try again :")
+            while (result == "wrong input"):
+                
+                continue
+            game.play()
+                
+
+               
+
+                    
+game = Game() 
+    
 game.play()
+
---
+++ rps_oop/new.py
@@ -0,0 +1,52 @@
+import random
+
+choicess = ["rock","paper","scissor"]
+
+class Game:
+
+    def compare_choice(self,player_1,player_2):
+        if player_1 == "rock" and player_2 == "paper" or player_1 == "paper" and player_2 == "scissor" or player_1 == "scissor" and player_2 == "rock":
+            return "player_2 "
+        
+        elif player_1 == "rock" and player_2 == "scissor" or player_1 == "scissor" and player_2 == "paper" or player_1 =="paper" and player_2 == "rock":
+            return "player_1 "
+                
+        elif player_1 == player_2:
+            return "tie"
+
+
+    def choose_1(self) :
+        
+        player_1_choose = input("choose from rock , paper and scissor  : ")
+        return player_1_choose
+    
+    def choose_2(self):
+        player_2_choose = random.choice(choicess)
+        return player_2_choose
+
+    
+    def __init__(self,player_1_ch,player_2_ch) :
+        
+        player_1_ch = self.choose_1()
+        player_2_ch = self.choose_2()
+        result = self.compare_choice(player_1_ch,player_2_ch)
+
+       
+        
+        if result == "player_2 ":
+            print("player_1 choose", player_1_ch ,"and player_2 choose" , player_2_ch)
+            print("player_2 wins")
+
+        elif  result == "player_1 ":
+            print("player_1 choose" ,player_1_ch ,"and player_2 choose" , player_2_ch) 
+            print("player_1 wins")
+
+        elif result == "tie":
+            print("player_1 choose" ,player_1_ch ,"and player_2 also choose"  ,player_2_ch)
+            print("both choose same its tie")
+
+                    
+game = Game("","") 
+    
+
+
